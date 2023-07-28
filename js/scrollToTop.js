ddaec61b61600dd90142e4187d2747b2a7a8493d
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-07-28 19:56:12 +0300

    encapsulation

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,14 +1,13 @@
-const topBtn = document.querySelector('#scrollToTopButton');
+const scrollToTop = () => {
+    const topBtn = document.querySelector('#scrollToTopButton');
+    topBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        seamless.scrollIntoView(document.querySelector(".header"), {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
+    })
+}
 
-topBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-    seamless.scrollIntoView(document.querySelector(".header"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
-    });
-
-
-})
-
-
+scrollToTop();
