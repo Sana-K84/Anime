ee006f84a0c0f0b829d02c988048e77e36e683e6
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-07-29 23:07:54 +0300

    add renderTopAnime

--- js/mainData.js
+++ js/mainData.js
@@ -1,10 +1,39 @@
 const mainData = () => {
 
+    const renderTopAnime = (array) => {
+        const wrapper = document.querySelector('.filter__gallery');
+
+        wrapper.innerHTML = '';
+        array.forEach(el => {
+            wrapper.insertAdjacentHTML('afterbegin',
+
+                `<div class="product__sidebar__view__item set-bg mix day years" data-setbg="${el.image}">
+                                    <div class="ep">${el.rating} /10</div>
+                                    <div class="view"><i class="fa fa-eye"></i> ${el.views}</div>
+                                    <h5><a href="/anime-details.html">${el.title}</a></h5>
+                                </div>`
+
+            )
+        });
+        wrapper.querySelectorAll('.set-bg').forEach(element => {
+            element.style.backgroundImage = `url(${element.dataset.setbg})`;
+        });
+
+    }
+
+
+
     fetch('https://anime-f9cc3-default-rtdb.firebaseio.com/anime.json')
         .then((response) => {
             return response.json()
         })
-        .then(data => console.log(data))
+        .then(data => {
+            const ganres = new Set;
+
+            renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+            console.log(data)
+
+        })
 }
 
 mainData();
