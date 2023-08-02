891acac0dbb38808e0a13d6fcbb8ff98014d61d5
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-08-02 20:39:34 +0300

    ganre

--- js/mainData.js
+++ js/mainData.js
@@ -8,8 +8,9 @@ const mainData = () => {
         ganres.forEach(ganre => {
             const productBlock = document.createElement('div');
             const listBlock = document.createElement('div');
+            const list = array.filter(item => item.ganre === ganre);
             listBlock.classList.add('row');
-
+            productBlock.classList.add('mb-5')
             wrapper.append(productBlock);
             productBlock.insertAdjacentHTML('afterbegin', `                    <div class="trending__product">
             <div class="row">
@@ -24,9 +25,33 @@ const mainData = () => {
                                 class="arrow_right"></span></a>
                     </div>
                 </div>
+            </div>`);
+            list.forEach(item => {
+                const tagsBlock = document.createElement('ul');
+                item.tags.forEach(el => {
+                    tagsBlock.insertAdjacentHTML('afterbegin', `<li>${el}</li>`)
+                })
+
+                listBlock.insertAdjacentHTML('afterbegin', `
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="${item.image}">
+                        <div class="ep">${item.rating} / 10</div>
+                        <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                    </div>
+                    <div class="product__item__text">
+${tagsBlock.outerHTML}
+                        <h5><a href="/anime-details.html">${item.title}</a>
+                        </h5>
+                    </div>
+                </div>
             </div>`)
+            })
             productBlock.append(listBlock);
             wrapper.append(productBlock);
+            wrapper.querySelectorAll('.set-bg').forEach(element => {
+                element.style.backgroundImage = `url(${element.dataset.setbg})`;
+            });
         })
 
     }
